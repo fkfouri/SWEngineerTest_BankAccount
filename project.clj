62254561ba6dec4d298a792f0245e52adff7f843
fkfouri <fabio.kfouri@embraer.com.br>
2019-10-12 16:25:47 -0300

    Estudo de Clojure

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject bank "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [enlive "1.1.6"]
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [
+		[cider/cider-nrepl "0.21.1"]
+    ]			 
+  :main ^:skip-aot bank.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+
+)
---
+++ src/bank/core.clj
@@ -0,0 +1,86 @@
+(ns bank.core
+  (:gen-class))
+
+
+(require '[net.cgrand.enlive-html :as enlive])
+(require '[clojure.string :as s])
+;; let's require ourselves a pretty printing function - pprint
+;; we'll be looking at a lot of data, might get messy
+(require '[clojure.pprint :refer [pprint]])
+(require '[clojure.data.json :as json])
+(require '[clojure.java.io :as io])
+
+
+(def availableLimit (atom nil))
+
+
+(defn renew  [& args]
+  (require '[bank.core :reload :all])
+)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+(renew)
+
+  (println "Hello, World again! (require '[bank.core :reload :all])"))
+
+(defn string-keys-to-symbols [map]
+(reduce #(assoc %1 (-> (key %2) keyword) (val %2)) {} map))
+
+;;verifica se eh uma conta
+(defn is-account [json]
+  (some? (get-in json ["account"]))
+)
+
+;;verifica se eh uma conta ativa
+(defn is-account-active [json]
+  (if (= (get-in json ["account" "activeCard"]) true) 
+      true 
+      false
+  )
+)
+
+;;obtem o limite de uma conta ativa
+(defn account-limit [json]
+  (if (= (is-account-active json) true)
+      (def availableLimit  (get-in json ["account" "availableLimit"]))
+      (def availableLimit nil)
+  )
+) 
+ 
+ 
+(defn read1 [& args]
+  (renew)
+  (def json (json/read-str (slurp "src/bank/oper1.json")))
+  ;;(println (get-in json ["account"]))
+  
+  
+  ;;verifica se existe account 
+  (println "is-account:" (is-account json))
+  (println "account-limit:" (account-limit json))
+  
+  
+  ;;(if (< some? 100) "yes" "no"))
+  ;;(def availableLimit  (get-in json ["account" "availableLimit"]))
+  (println "availableLimit:" availableLimit)
+  ;;
+)
+
+;;obtem o limite de uma conta ativa
+(defn account-limit [json]
+  (if (= (is-account-active json) true)
+      (def availableLimit  (get-in json ["account" "availableLimit"]))
+      (def availableLimit nil)
+  )
+) 
+ 
+(defn read2 [& args]
+  (read1)
+  (def json (json/read-str (slurp "src/bank/oper2.json")))
+  (println (get-in json ["transaction"]))
+
+  (def amount (get-in json ["transaction" "amount"]))
+  (println "new availableLimit:" (- availableLimit amount))
+  ;;
+)
