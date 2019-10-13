3a8b90b5f170b414317f21bb15972ec678fdee53
fkfouri <kfouri.fabio@gmail.com>
2019-10-13 08:46:44 -0300

    Desenvolvimento

--- src/bank/core.clj
+++ src/bank/core.clj
@@ -1,7 +1,6 @@
 (ns bank.core
   (:gen-class))
 
-
 (require '[net.cgrand.enlive-html :as enlive])
 (require '[clojure.string :as s])
 ;; let's require ourselves a pretty printing function - pprint
@@ -9,78 +8,81 @@
 (require '[clojure.pprint :refer [pprint]])
 (require '[clojure.data.json :as json])
 (require '[clojure.java.io :as io])
-
-
 (def availableLimit (atom nil))
 
-
 (defn renew  [& args]
-  (require '[bank.core :reload :all])
-)
+  (require '[bank.core :reload :all]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-(renew)
-
+  (renew)
   (println "Hello, World again! (require '[bank.core :reload :all])"))
 
-(defn string-keys-to-symbols [map]
-(reduce #(assoc %1 (-> (key %2) keyword) (val %2)) {} map))
 
 ;;verifica se eh uma conta
 (defn is-account [json]
-  (some? (get-in json ["account"]))
-)
+  (some? (get-in json ["account"])))
+
 
 ;;verifica se eh uma conta ativa
 (defn is-account-active [json]
-  (if (= (get-in json ["account" "activeCard"]) true) 
-      true 
-      false
-  )
-)
+  (if (= (get-in json ["account" "activeCard"]) true)
+    true
+    false))
 
 ;;obtem o limite de uma conta ativa
 (defn account-limit [json]
   (if (= (is-account-active json) true)
-      (def availableLimit  (get-in json ["account" "availableLimit"]))
-      (def availableLimit nil)
-  )
-) 
- 
- 
+    (def availableLimit  (get-in json ["account" "availableLimit"]))
+    (def availableLimit nil)))
+
 (defn read1 [& args]
   (renew)
-  (def json (json/read-str (slurp "src/bank/oper1.json")))
+  ;leitura do arquivo json
+  (def json (json/read-str (slurp "src/bank/oper1.json"))) 
+
   ;;(println (get-in json ["account"]))
-  
-  
+
   ;;verifica se existe account 
   (println "is-account:" (is-account json))
+
+  ;;exibe o limite da conta
   (println "account-limit:" (account-limit json))
-  
-  
+
+
   ;;(if (< some? 100) "yes" "no"))
+
   ;;(def availableLimit  (get-in json ["account" "availableLimit"]))
   (println "availableLimit:" availableLimit)
   ;;
-)
+  )
+
+
+
 
 ;;obtem o limite de uma conta ativa
 (defn account-limit [json]
   (if (= (is-account-active json) true)
-      (def availableLimit  (get-in json ["account" "availableLimit"]))
-      (def availableLimit nil)
-  )
-) 
- 
+    (def availableLimit  (get-in json ["account" "availableLimit"]))
+    (def availableLimit nil)))
+
 (defn read2 [& args]
   (read1)
   (def json (json/read-str (slurp "src/bank/oper2.json")))
   (println (get-in json ["transaction"]))
-
   (def amount (get-in json ["transaction" "amount"]))
   (println "new availableLimit:" (- availableLimit amount))
+
   ;;
+  )
+
+
+
+;equivalente o stdin para receber as referencias json
+(defn operations [& args]
+  (doseq [line (line-seq (java.io.BufferedReader. *in*))]
+
+    (println line)
+  )
 )
